3ad5399c4457c4048a746b68fe8c77050818bbf6
M0o2leesea <mthulisinkuna@icloud.com>
2024-02-07 14:53:18 +0900

    first commit

---
+++ initials.py
@@ -0,0 +1,19 @@
+# Mthulisi Nkuna
+# Fun fact: Had an opportunity to study computer science at high school but didn't take it. Now here am I. 
+print("M     M  TTTTTTTTTT")
+print("MM   MM      T")
+print("MM   MM      T")
+print("M  M  M      T")
+print("M     M      T")
+print("M     M      T")
+
+surname = """
+N     N  K     K  U     U    NN  N    AA  A
+NN    N  K    K   U     U  N     N  A    AA
+N  N  N  K  K     U     U  N     N  A     A
+N    NN  KK       U     U  N     N  A     A
+N     N  K  K     U     U  N     N  A    AA
+N     N  K     K    UUU    N     N    AA  A
+     """
+
+print(surname)
